e909f1eb6052c9ab5c1baeb9086efdc49e0dfb5c
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2021-03-17 21:45:20 +0200

    Додає build

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import React, { useState, useEffect } from "react";
 import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
+import AddFavorites from "./components/AddFavorites";
+import RemoveFavorites from "./components/RemoveFavorites";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-const debounce = require("debounce");
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
-
-  const getMovieRequest = async () => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=6fa36c8f`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    data.Search && setMovies(data.Search);
-  };
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(
     (searchValue) => {
@@ -26,6 +19,38 @@ const App = () => {
     },
     [searchValue]
   );
+  useEffect(() => {
+    const myFavorites = JSON.parse(
+      localStorage.getItem("react-movie-app-favorites")
+    );
+    setFavorites(myFavorites);
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-favorites", JSON.stringify(items));
+  };
+
+  const getMovieRequest = async () => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=6fa36c8f`;
+    const response = await fetch(url);
+    const data = await response.json();
+    data.Search && setMovies(data.Search);
+  };
+
+  const addFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie];
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  };
+
+  const removeFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites].filter(
+      (favorite) => favorite.imdbID !== movie.imdbID
+    );
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  };
+
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
@@ -33,7 +58,21 @@ const App = () => {
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
-        <MovieList movies={movies} />
+        <MovieList
+          movies={movies}
+          handleFavritesClick={addFavoriteMovie}
+          favoriteComponent={AddFavorites}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourites" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favorites}
+          handleFavritesClick={removeFavoriteMovie}
+          favoriteComponent={RemoveFavorites}
+        />
       </div>
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 const MovieList = (props) => {
+  const FavoriteComponent = props.favoriteComponent;
   return (
     <>
       {props.movies.map((movie, index) => {
         return (
-          <div key={index} className="d-flex justify-content-start m-3">
+          <div
+            key={index}
+            className="image-container d-flex justify-content-start m-3"
+          >
             <img src={movie.Poster} alt="movie" />
+            <div
+              onClick={() => props.handleFavritesClick(movie)}
+              className="overlay d-flex align-items-center justify-content-center"
+            >
+              <FavoriteComponent />
+            </div>
           </div>
         );
       })}
